a354aebf3ebf336d9703d23359bce2bf8caadbeb
Rob Brown <brown4homes@yahoo.com>
2019-02-27 12:55:18 -0800

    Edit, destroy, and add notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,7 +6,6 @@ class NotesController < ApplicationController
   end
 
   def show
-
   end
 
   def new
@@ -24,15 +23,19 @@ class NotesController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @note.destroy
+    redirect_to notes_path
   end
 
   private
